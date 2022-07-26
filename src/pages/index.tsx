34baa11fdef4325259d3ae152eb94fc378c09e15
Nguyễn Ngọc Anh <anhnn02.it@gmail.com>
2022-07-27 00:02:11 +0700

    feat: layout homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,23 @@ import styles from "../styles/client/Home.module.scss";
 const Home: NextPage = () => {
     return (
         <>
-        <div className="tw-bg-green-30 tw-h-[1000px]"></div>
-            <h1 className={styles['demo']}>Home</h1>
-            <i className="bi bi-wrench-adjustable-circle-fill"></i>
-            <Button.Transparent content={"Add"} />
-            <Button.Fill content={<Icon.Search />} />
+            <div className={styles['homepage']}>
+                <div className={styles['section']}>
+                    Slide
+                </div>
+                <div className={styles['section']}>
+                    Slide
+                </div>
+                <div className={styles['section']}>
+                    Slide
+                </div>
+                <div className={styles['section']}>
+                    Slide
+                </div>
+                <div className={styles['section']}>
+                    Slide
+                </div>
+            </div>
         </>
     )
 };
