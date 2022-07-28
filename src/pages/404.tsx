cba51dd10bf5408abd8e4a46b29cfaf1e03db03c
cancer21072002 <92455145+cancer21072002@users.noreply.github.com>
2022-07-28 22:33:50 +0700

    NEXTJS-68-fe-client-page-404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
-import LayoutEmpty from '../components/Layout/empty'
-import Button from '@/components/Button'
-import Link from 'next/link'
-
+import React from "react";
+import LayoutEmpty from "../components/Layout/empty";
+import Button from "@/components/Button";
+import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="not-found">
-      <img  className="img404" src="https://bonik-react.vercel.app/assets/images/illustrations/404.svg" alt=""  />
-       <Button.Fill content={"Go to Home"} />
-      
+    <div className="tw-h-screen tw-flex tw-text-center tw-p-3">
+      <div className="tw-m-auto">
+        <img
+          className="tw-inline-block tw-w-96"
+          src="https://bonik-react.vercel.app/assets/images/illustrations/404.svg"
+          alt=""
+        />
+        <div className="tw-my-6">
+          <Link href="/">
+            <Button.Fill content={"Go to Home"} />
+          </Link>
+        </div>
+      </div>
     </div>
   );
-}
- NotFound.Layout = LayoutEmpty
+};
+NotFound.Layout = LayoutEmpty;
 export default NotFound;
