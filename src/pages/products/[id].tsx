32f9751be8275650b7946393c2ea2c3d3e7078d3
Nguyễn Ngọc Anh <anhnn02.it@gmail.com>
2022-07-30 15:01:43 +0700

    NEXTJS-61 feat: add to cart

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,5 +1,6 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Product.module.scss";
 import Icon from "@/components/Icon";
 import Button from "@/components/Button";
@@ -8,14 +9,53 @@ import ListProduct from "@/components/client/shop/ListProduct";
 import { formatPercent, formatPrice } from "@/utils/formatNumber";
 import Link from "next/link";
 import { path } from "@/constants";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { addItemToCart } from "@/features/cart/cart.slice";
+import { toast } from 'react-toastify';
 
 type ProductProps = {
   product: any[];
   related: any[];
 };
 const DetailProduct = ({ product, related }: ProductProps) => {
-  if (!product) return null;
-  console.log(product);
+  const [quantity, setQuantity] = useState(1)
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  const dispatch = useDispatch();
+
+  const handleAddToCart = (data) => {
+    const itemCart = {
+      idInCart: product._id + '_' + data.size,
+      idPro: product._id,
+      size: data.size,
+      img: product.img,
+      quantity: quantity * 1,
+      name: product.name,
+      desc: product.desc,
+      regularPrice: product.regularPrice * 1,
+      salePrice: product.salePrice * 1,
+      categoryProduct: product.category,
+      total: (product.salePrice) ? product.salePrice * quantity : product.regularPrice * quantity
+    }
+    dispatch(addItemToCart(itemCart))
+    setQuantity(1)
+    toast.success("Add to cart successfully!", {
+      position: 'top-center'
+    })
+  }
+
+  const incrementQuantity = () => {
+    setQuantity(quantity + 1)
+  }
+  const decrementQuantity = () => {
+    if (quantity < 2) {
+      setQuantity(1)
+    } else {
+      setQuantity(quantity - 1)
+    }
+
+  }
+  if (!product) return <div className="">Loading...</div>;
   return (
     <div className={styles["container"]}>
       <div className={styles["grid"]}>
@@ -37,7 +77,7 @@ const DetailProduct = ({ product, related }: ProductProps) => {
           </div>
         </div>
 
-        <form className={styles["container-text"]}>
+        <form className={styles["container-text"]} onSubmit={handleSubmit(handleAddToCart)}>
           <h3 className={styles["grid-text"]}>{product.name}</h3>
           <div className={styles["grid-pad"]}>
             <span className={styles["grid-info"]}> Category: </span>
@@ -81,56 +121,27 @@ const DetailProduct = ({ product, related }: ProductProps) => {
           <div className={styles["check"]}>
             <span>Size: &nbsp; </span>
             <div className={styles["size-product-detail"]}>
-              <div className={`${styles['form-group']} ${styles['form-group-35']}`}>
-                <input type="radio" id="size-35" name="radio-size" />
-                <label htmlFor="size-35"></label>
-              </div>
-              <div className={`${styles['form-group']} ${styles['form-group-36']}`}>
-                <input type="radio" id="size-36" name="radio-size" />
-                <label htmlFor="size-36"></label>
-              </div>
-              <div className={`${styles['form-group']} ${styles['form-group-37']}`}>
-                <input type="radio" id="size-37" name="radio-size" />
-                <label htmlFor="size-37"></label>
-              </div>
-              <div className={`${styles['form-group']} ${styles['form-group-38']}`}>
-                <input type="radio" id="size-38" name="radio-size" />
-                <label htmlFor="size-38"></label>
-              </div>
-              <div className={`${styles['form-group']} ${styles['form-group-39']}`}>
-                <input type="radio" id="size-39" name="radio-size" />
-                <label htmlFor="size-39"></label>
-              </div>
-              <div className={`${styles['form-group']} ${styles['form-group-40']}`}>
-                <input type="radio" id="size-40" name="radio-size" />
-                <label htmlFor="size-40"></label>
-              </div>
-              <div className={`${styles['form-group']} ${styles['form-group-41']}`}>
-                <input type="radio" id="size-41" name="radio-size" />
-                <label htmlFor="size-41"></label>
-              </div>
-              <div className={`${styles['form-group']} ${styles['form-group-42']}`}>
-                <input type="radio" id="size-42" name="radio-size" />
-                <label htmlFor="size-42"></label>
-              </div>
-              <div className={`${styles['form-group']} ${styles['form-group-43']}`}>
-                <input type="radio" id="size-43" name="radio-size" />
-                <label htmlFor="size-43"></label>
-              </div>
+              {product.size.map((size, index) => (
+                <div key={index} className={`${styles['form-group']} ${styles[`form-group-${size}`]}`}>
+                  <input type="radio" value={size} id={`size_${index}`} {...register('size', { required: true })} />
+                  <label htmlFor={`size_${index}`}></label>
+                </div>
+              ))}
             </div>
           </div>
+          {errors.size && (<div className="my-error !tw-text-lg">Please choose a size!</div>)}
           <div className={styles["number"]}>
-            <Button.Transparent
-              className={styles["number-iconnr"]}
-              content={<Icon.Dash />}
-            />
-            <Button.None content="1" />
-            <Button.Transparent
-              className={styles["number-iconnl"]}
-              content={<Icon.PlusRegular />}
-            />
+            <button type="button" className={styles['cart-sidebar__button']}
+              onClick={() => decrementQuantity()}>
+              <Icon.Dash />
+            </button>
+            <span>{quantity}</span>
+            <button type="button" className={styles['cart-sidebar__button']}
+              onClick={() => incrementQuantity()}>
+              <Icon.PlusRegular />
+            </button>
           </div>
-          <Button.Fill className="tw-px-8" content={"Add to cart"} />
+          <Button.Fill className="tw-px-10" content={"Add to cart"} />
           <div className={styles["grid-heith"]}>
             <span className={styles["grid-info"]}> Sold By: </span>
             <span>Mobile Store</span>
