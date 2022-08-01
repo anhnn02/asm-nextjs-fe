191bf2f3b31ead785e6fb84c2ab1c64b2c99de05
cancer21072002 <92455145+cancer21072002@users.noreply.github.com>
2022-08-02 03:41:00 +0700

    NEXTJS-69

---
+++ src/components/client/category/category.tsx
@@ -0,0 +1,58 @@
+import useCate from '@/hooks/use-category';
+import { Component} from 'react'
+// import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+ import { Swiper, SwiperSlide } from "swiper/react";
+ import React from 'react';
+ // Import Swiper styles
+ import "swiper/css";
+import Card from 'react-bootstrap/Card';
+
+import styles from './Category.module.scss'
+import Slider from "react-slick";
+import { Slide } from 'react-slideshow-image';
+
+
+
+type Props = {
+  
+};
+
+function Category({}: Props) {
+      const { data, error } = useCate();
+      console.log(data);
+
+      if (!data) return <div>Loading...</div>;
+      if (error) return <div>Failed to loading</div>;
+  return (
+    <>
+      <h1 className="font-bold text-[20px] py-[20px]">category</h1>
+      <Slide>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <div className="swiper-wrapper">
+            {data.map((item, index) => (
+              <Card
+                className="swiper-slide"
+                style={{ width: "18rem" }}
+                key={index}
+              >
+                <Card.Body>
+                  <SwiperSlide className={styles["box-category"]}>
+                    {item.name}
+                  </SwiperSlide>
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
+        </Swiper>
+      </Slide>
+    </>
+  );
+}
+
+export default Category
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import Button from "@/components/Button";
+import Category from "@/components/client/category/category";
 import Banner from "@/components/client/Slide banner/Slide";
 import Icon from "@/components/Icon";
 import Product from "@/features/products/product.component";
@@ -7,17 +8,19 @@ import { NextPage } from "next";
 import { ToastContainer, toast } from "react-toastify";
 import styles from "../styles/client/Home.module.scss";
 
+import Card from "react-bootstrap/Card";
 
 const Home: NextPage = () => {
   return (
     <div className={styles["homepage"]}>
       <div className={styles["section"]}>Slide</div>
-      <div className={styles["section"]}>
-        <Banner />
-      </div>
-      <div className={styles["section"]}>
-      </div>
+
+      <Banner />
+
+      <div className={styles["section"]}></div>
+
       <div className={styles["section"]}>Slide</div>
+      <Category />
       <div className={styles["section"]}>Slide</div>
     </div>
   );
