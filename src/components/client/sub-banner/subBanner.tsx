cd59c4507182e1570f342e6a83235b436f9ce4f6
cancer21072002 <92455145+cancer21072002@users.noreply.github.com>
2022-08-03 07:25:26 +0700

    img

--- src/components/client/sub-banner/subBanner.tsx
+++ src/components/client/sub-banner/subBanner.tsx
@@ -8,15 +8,23 @@ const SubBanner = (props: Props) => {
   return (
     <>
       <div className={styles["banner"]}>
-        <div className={styles["banner01"]}> <span>
-          <img src="https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanners%2Fbanner-1.png&w=1920&q=75" />
-        </span>
+        <div className={styles["banner01"]}>
+          {" "}
+          <span>
+            <img
+              className={styles["img1"]}
+              src="https://theme.hstatic.net/200000386519/1000741676/14/categorybanner_1_img.jpg?v=6"
+            />
+          </span>
         </div>
-       <div className={styles["banner02"]}><span >
-          <img src="https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanners%2Fbanner-2.png&w=1920&q=75" />
-        </span>
+        <div className={styles["banner02"]}>
+          <span>
+            <img
+              className={styles["img2"]}
+               src="https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanners%2Fbanner-2.png&w=1920&q=75"
+            />
+          </span>
         </div>
-        
       </div>
     </>
   );
