6586600e52ece699f8e126defb0d666d89727ef9
Nguyễn Ngọc Anh <anhnn02.it@gmail.com>
2022-08-08 10:05:01 +0700

    change port

--- src/api/instance.tsx
+++ src/api/instance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:3002/api",
+    baseURL: "http://localhost:3001/api",
 });
 instance.interceptors.response.use(
     function (response) {
